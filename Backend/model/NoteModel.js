d742627b8b064aa5cdae7cceecf92facf9cd0311
Johan <bangjev190@gmail.com>
2025-03-01 02:49:52 +0700

    ganti .string ke .text

--- Backend/model/NoteModel.js
+++ Backend/model/NoteModel.js
@@ -3,7 +3,8 @@ import db from "../config/Database.js"
 
 const Note = db.define('note', {
     title: Sequelize.STRING,
-    content: Sequelize.STRING,},{
+    content: Sequelize.TEXT,
+    },{
         freezeTableName: true,
         createdAt: 'tanggal_buat',
         updatedAt: 'tanggal_ubah'
