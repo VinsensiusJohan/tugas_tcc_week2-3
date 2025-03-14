bb7a4c7a97b20ad26dfcf6125defde69fed2f600
Johan <bangjev190@gmail.com>
2025-03-14 19:21:57 +0700

    ubah kofigurasi ip pw db

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('note', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize('note', 'root', '12345', {
+    host: '34.173.12.206',
     dialect: 'mysql'
 }); 
 
