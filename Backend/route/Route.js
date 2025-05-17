3e43d16fb8b1b3b768f3d9fb920a0ca546ceddea
Johan <bangjev190@gmail.com>
2025-05-18 01:30:55 +0700

    nambah endpoint dan verifikasi

--- Backend/route/Route.js
+++ Backend/route/Route.js
@@ -1,11 +1,20 @@
 import express from "express";
+import { verifyToken } from "../middleware/verifyToken.js";
 import { createNote, getNote, updateNote, deleteNote } from "../controller/NoteController.js";
-
+import {
+    registerUser,
+    loginUser,
+    logoutUser
+} from "../controller/UserController.js";
 const router = express.Router();
 
-router.get('/note', getNote);
-router.post('/add', createNote);
-router.put('/note-update/:id', updateNote);
-router.delete('/note-delete/:id', deleteNote);
+router.post("/login", loginUser);
+router.post("/register", registerUser);
+router.get("/logout", logoutUser);
+
+router.get('/note', verifyToken, getNote);
+router.post('/add', verifyToken, createNote);
+router.put('/note-update/:id', verifyToken, updateNote);
+router.delete('/note-delete/:id', verifyToken, deleteNote);
 
 export default router;
