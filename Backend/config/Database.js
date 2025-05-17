e88b19bfecc5f34908129eaf25a1405cf8e9ffcc
Johan <bangjev190@gmail.com>
2025-05-18 01:25:37 +0700

    ganti pake .env

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize('note', 'root', '12345', {
-    host: '34.173.12.206',
+dotenv.config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 }); 
 
